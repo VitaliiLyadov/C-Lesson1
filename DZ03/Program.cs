107bc5b96efd6a9e5f3dc5e1021dbf1ab933c7de
Vitalii <LVitek2506@yandex.ru>
2023-03-25 15:51:34 +0500

    Задание 3

---
+++ DZ03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Укажите число > ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    System.Console.WriteLine("Число четное");
+}
+else
+{
+    System.Console.WriteLine("Число нечетное");
+}
