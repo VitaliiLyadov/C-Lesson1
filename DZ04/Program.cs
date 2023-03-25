afd8284a1d5be4d1339a307e2cc06f289f0c146f
Vitalii <LVitek2506@yandex.ru>
2023-03-25 15:51:54 +0500

    Задание 4

---
+++ DZ04/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Укажите число > ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= N)
+{
+    if (i % 2 == 0)
+    {
+        System.Console.WriteLine(i);
+    }
+    i++;
+}
